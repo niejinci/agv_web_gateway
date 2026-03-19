194f6c5a026d3cb6314beeaf4ea89056fd55f8b0
whillys <fox.nie@163.com>
2026-03-19 10:07:29 +0800

    feat: 修改网关的端口为9090
    【背景】
    
    在 AGV Web 控制台中新增功能入口，实现一键跳转至【AGV Web 3D 可视化系统】，刘工反馈8080端口已被占用，因此修改网关的端口。
    
    【修改】
    - 修改 websocketpp 实例listen() 的端口

--- src/main.cpp
+++ src/main.cpp
@@ -4,8 +4,8 @@
 int main(int argc, char* argv[]) {
     try {
         AgvWebGateway gateway;
-        // 连接到你的 agv server (假设是本机 9034)，对外提供 8080 WebSocket 服务
-        gateway.run("127.0.0.1", 9034, 8080);
+        // 连接到你的 agv server (假设是本机 9034)，对外提供 9090 WebSocket 服务
+        gateway.run("127.0.0.1", 9034, 9090);
     } catch (const std::exception& e) {
         std::cerr << "网关发生异常: " << e.what() << std::endl;
     }
